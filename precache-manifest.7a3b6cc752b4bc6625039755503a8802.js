6dcb09eede2c8db0196dd4a3f6b23313e4076f9b
Daya Lu <dayalu@jhu.edu>
2019-10-28 20:22:58 +0800

    Updates

---
+++ precache-manifest.7a3b6cc752b4bc6625039755503a8802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a56b08218fe11ac2117fc72d95e481",
+    "url": "/dayaswebsite/index.html"
+  },
+  {
+    "revision": "2642cb309d9d93e2c209",
+    "url": "/dayaswebsite/static/css/main.7d2ab510.chunk.css"
+  },
+  {
+    "revision": "fed4fb9d9e2ff70f0d5c",
+    "url": "/dayaswebsite/static/js/2.0a839049.chunk.js"
+  },
+  {
+    "revision": "2642cb309d9d93e2c209",
+    "url": "/dayaswebsite/static/js/main.1d3091a9.chunk.js"
+  },
+  {
+    "revision": "e668b71f77c54c75b40b",
+    "url": "/dayaswebsite/static/js/runtime-main.f4873834.js"
+  }
+]);
